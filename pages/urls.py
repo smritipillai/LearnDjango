852075e2fea1082e8f12128125e6288b3869bd11
Smriti <smritipillai.smriti@gmail.com>
2019-08-25 23:48:05 +0530

    base.html added

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homePageView.as_view(), name='home'),
+    path('home/', views.homePageView.as_view(), name='home'),
+    path('about/', views.AboutPageView.as_view(), name='about')
 ]
