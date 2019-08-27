a22da437dcc5421215842748d166580327b9faee
Smriti <smritipillai.smriti@gmail.com>
2019-08-27 20:50:31 +0530

    Edited urls.py

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Post
+
+class PostModelTest(TestCase):
+    def setUp(Test):
+        Post.objects.create(text = 'some text')
+        
+    def testSetContent(self):
+        post = Post.objects.get(id=1)
+        name = f'{post.text}'
+        self.assertEqual(name, 'sometext')
